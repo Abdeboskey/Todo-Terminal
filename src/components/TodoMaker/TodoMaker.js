f6a3e53e3d8870e51b7a6e2b6ac3e9d6c660b2e5
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-09-28 04:39:21 -0600

    Create todomaker component

---
+++ src/components/TodoMaker/TodoMaker.js
@@ -0,0 +1,41 @@
+import classes from './TodoMaker.module.scss'
+import React, { useState } from 'react'
+
+const TodoMaker = ({ addTodo }) => {
+  const [ input, setInput ] = useState('')
+  const [ error, setError ] = useState('')
+
+  const createTodo = event => {
+    event.preventDefault()
+    if (!input) {
+      setError('Enter Text Here')
+    } else {
+      addTodo(input)
+      setInput('')
+      setError('')
+    }
+  }
+
+  return (
+    <form 
+      className={classes.container}
+      onSubmit={createTodo}
+      autoComplete="off"
+    >
+      <label htmlFor="make-todo" className={classes.label}>
+        What would you like to do?
+      </label>
+      <input
+        className={classes.input}
+        name="make-todo"
+        type="text"
+        placeholder={error}
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button className={classes.button}>Make Todo</button>
+    </form>
+  )
+}
+
+export default TodoMaker
