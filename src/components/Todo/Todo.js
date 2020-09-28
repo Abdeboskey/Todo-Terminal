0a721d26d0e5c37aaa2c7ae8523aeb1308ae866b
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-09-28 05:26:30 -0600

    Move datetime to todomaker

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,13 +1,11 @@
 import React from 'react'
-import { DateTime } from 'luxon'
 import classes from './Todo.module.scss'
-const dt = DateTime.local()
 
 const Todo = ({ todo }) => {
   return (
     <div className={classes.TodoCard}>
-      <h2>{todo[0].toUpperCase() + todo.substring(1) + '.'}</h2>
-      <p>Added: {dt.toLocaleString(DateTime.DATETIME_MED)}</p>
+      <h2>{todo.todo[0].toUpperCase() + todo.todo.substring(1) + '.'}</h2>
+      <p>Added: {todo.dateTime}</p>
       <button>Complete</button>
     </div>
   )
