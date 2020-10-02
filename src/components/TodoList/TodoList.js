933a09eb354a4a4eb0b3a854d3005061f3f0d90a
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-10-02 17:52:27 -0600

    Create classname for todolist

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Todo from '../Todo/Todo'
+import classes from './TodoList.module.scss'
 
 const TodoList = ({ todos }) => {
   const allTodos = todos.map((todo, index) => <Todo key={index} todo={todo} />)
 
   return (
-    <section>
+    <section className={classes.TodoList}>
       {allTodos.reverse() || <h2>You have not added any todos</h2>}
     </section>
   )
