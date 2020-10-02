e13a4005f9d16a3238a4197fd15565dc2d14cbad
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-10-02 17:51:57 -0600

    Create classname for timestamp

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -5,7 +5,7 @@ const Todo = ({ todo }) => {
   return (
     <div className={classes.TodoCard}>
       <h2>{todo.todo[0].toUpperCase() + todo.todo.substring(1) + '.'}</h2>
-      <p>Added: {todo.timeStamp}</p>
+      <p className={classes.timeStamp}>Added: {todo.timeStamp}</p>
       <button className={classes.button}>Complete</button>
     </div>
   )
