1d4867d92f15943fd943359297de12c5b9527481
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-10-02 18:13:33 -0600

    Pass completetodo down as props

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -2,8 +2,10 @@ import React from 'react'
 import Todo from '../Todo/Todo'
 import classes from './TodoList.module.scss'
 
-const TodoList = ({ todos }) => {
-  const allTodos = todos.map((todo, index) => <Todo key={index} todo={todo} />)
+const TodoList = ({ todos, completeTodo }) => {
+  const allTodos = todos.map((todo, index) => {
+    return <Todo key={index} todo={todo} completeTodo={completeTodo} />
+  })
 
   return (
     <section className={classes.TodoList}>
