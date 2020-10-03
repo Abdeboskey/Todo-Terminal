6ea0bb8511bce0756cd80504783908864060c084
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-10-02 18:13:47 -0600

    Add complete todo to button

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import classes from './Todo.module.scss'
 
-const Todo = ({ todo }) => {
+const Todo = ({ todo, completeTodo }) => {
   return (
     <div className={classes.TodoCard}>
       <h2>{todo.todo[0].toUpperCase() + todo.todo.substring(1) + '.'}</h2>
       <p className={classes.timeStamp}>Added: {todo.timeStamp}</p>
-      <button className={classes.button}>Complete</button>
+      <button
+        className={classes.button}
+        onClick={() => completeTodo(todo.id)}
+      >
+        Complete
+      </button>
     </div>
   )
 }
