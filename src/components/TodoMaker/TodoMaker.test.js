36b7ee8b1d4348376f72ced14d67c6da35493638
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-10-03 00:32:59 -0600

    Create first tests

---
+++ src/components/TodoMaker/TodoMaker.test.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import TodoMaker from './TodoMaker'
+import { fireEvent, render } from '@testing-library/react'
+import '@testing-library/jest-dom'
+
+describe('TodoMaker', () => {
+  it('should display the correct contents when rendered', () => {
+    const mockSaveTodo = jest.fn()
+    const { getByRole, getByText } = render(
+      <>
+        <TodoMaker saveTodo={mockSaveTodo} />
+      </>
+    )
+
+    const prompt = getByText(/what would you like to do?/i)
+    const input = getByRole('textbox')
+    const makeTodoButton = getByRole('button', { name: /make to-do/i })
+
+    expect(prompt).toBeInTheDocument()
+    expect(input).toBeInTheDocument()
+    expect(makeTodoButton).toBeInTheDocument()
+  })
+
+  it('should update the value of the form when typed into', () => {
+    const mockSaveTodo = jest.fn()
+    const { getByRole } = render(
+      <>
+        <TodoMaker saveTodo={mockSaveTodo} />
+      </>
+    )
+
+    const input = getByRole('textbox')
+    fireEvent.change(input, { target: { value: 'test the form' } })
+
+    expect(input.value).toEqual('test the form')
+  })
+})
