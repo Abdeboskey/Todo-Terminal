d43f57681b61071207260ee093d0a2a2fffcdc2e
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-10-03 01:42:17 -0600

    Create tests for app

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,9 +1,40 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import App from './App'
+import { fireEvent, render } from '@testing-library/react'
+import '@testing-library/jest-dom'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App', () => {
+  it('should allow a user to create a to-do', () => {
+    const { getByRole } = render(
+      <>
+        <App />
+      </>
+    )
+
+    const input = getByRole('textbox')
+    fireEvent.change(input, { target: { value: 'practice typing' } })
+
+    const makeTodoButton = getByRole('button', { name: /make to-do/i })
+    fireEvent.click(makeTodoButton)
+
+    const typingTodo = getByRole('heading', { name: /practice typing/i })
+    expect(typingTodo).toBeInTheDocument()
+  })
+
+  it('should allow a user to remove a to-do when completed', () => {
+    const { getByRole } = render(
+      <>
+        <App />
+      </>
+    )
+    
+    const todoHeading = getByRole('heading', { name: /practice typing/i })
+    expect(todoHeading).toBeInTheDocument()
+
+    const completeButton = getByRole('button', { name: /complete/i })
+    fireEvent.click(completeButton)
+    
+    expect(todoHeading).not.toBeInTheDocument()
+  })
+})
+
